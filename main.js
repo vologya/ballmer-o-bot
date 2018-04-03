f93fa0c94dd530a01d0e476a5a47dcd2c18524f7
Vologya <vologya@programmer.net>
2018-04-03 21:23:10 +0200

    Initial setup of a basic Discord Bot using discord.js

---
+++ main.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+let auth = require("./auth.json");
+client.login(auth.token);
